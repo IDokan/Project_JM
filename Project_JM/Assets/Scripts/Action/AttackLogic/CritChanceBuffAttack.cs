de2d96aedb0c1215182494818266a638838d4136
Sinil Kang <rtd99062@gmail.com>
2025-11-24 10:43:29 +0900

    #12 - Created skills of Bowman.
    CritChanceBuffAttack - An attack that gives critical chance bonus.
    
    CritDamageBuffAttack - An attack that gives critical damage bonus. It lasts until anyone died.
    
    CharacterCombatant - Add interfaces for buffs related to criticals.
    
    CharacterStatus - Add interfaces for buffs related to criticals. Add timed modifiers for duration buff for critical chance buffs. Now range of critical hit chance became from 0 to 1. 1 means 100%.
    
    CombatManager - Escape immediately for matchs of None color.
    
    CombotManager - Escape immediately for matchs of None color. Now critical range came to 0~1. 1 means 100%.

---
+++ Project_JM/Assets/Scripts/Action/AttackLogic/CritChanceBuffAttack.cs
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 11/23/2025 Sinil Kang
+// Project: Project JM - https://github.com/IDokan/Project_JM
+// File: CritChanceBuffAttack.cs
+// Summary: A skill that gives damage and critical chancebuff to attacker.
+
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CritChanceBuffAttack", menuName = "JM/Combat/AttackLogic/Ally/Crit Chance Buff Attack")]
+public class CritChanceBuffAttack : AttackLogic
+{
+    [SerializeField] protected int baseDamage = 6;
+    [SerializeField] protected float buffAmount = 0.2f;
+    [SerializeField] protected float duration = 5f;
+
+    public override IEnumerator Execute(AttackContext ctx)
+    {
+        ctx.Attacker?.AddBuffCritBonus(buffAmount, duration);
+        ctx.Target?.TakeDamage(baseDamage, ctx);
+
+        yield break;
+    }
+
+    public override Vector3 GetAttackerMotionOffset()
+    {
+        return new Vector3(-0.75f, 1.5f, 0f);
+    }
+
+    public override Vector3 GetTargetMotionOffset()
+    {
+        return new Vector3(1f, -0.1f, 0f);
+    }
+}
---
+++ Project_JM/Assets/Scripts/Action/AttackLogic/CritDamageBuffAttack.cs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 11/23/2025 Sinil Kang
+// Project: Project JM - https://github.com/IDokan/Project_JM
+// File: CritDamageBuffAttack.cs
+// Summary: A skill that gives damage and critical damage buff to attacker.
+
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CritDamageBuffAttack", menuName = "JM/Combat/AttackLogic/Ally/Crit Damage Buff Attack")]
+public class CritDamageBuffAttack : AttackLogic
+{
+    [SerializeField] protected int baseDamage = 9;
+    [SerializeField] protected float buffAmount = 0.3f;
+
+    public override IEnumerator Execute(AttackContext ctx)
+    {
+        ctx.Attacker?.AddBuffCritDamage(buffAmount);
+        ctx.Target?.TakeDamage(baseDamage, ctx);
+
+        yield break;
+    }
+
+    public override Vector3 GetAttackerMotionOffset()
+    {
+        return new Vector3(-1f, 2f, 0f);
+    }
+
+    public override Vector3 GetTargetMotionOffset()
+    {
+        return new Vector3(1.5f, -0.2f, 0f);
+    }
+}
