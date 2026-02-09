ffa06d6dac01fe8565b252944a009fef2767383e
Sinil Kang <rtd99062@gmail.com>
2026-02-09 15:40:21 +0900

    #23 - Finished adding VFXs to the Mage.
    MageBasicAttack, MageIceShards - Change function names to spawn magic circle to random position.
    
    MageChronoPulse, MageChronoPulseApproach, MageChronoPulseReturn - Adjusted Animation a bit.
    
    BlueSquare - Added scripts to spawn magic circle and chrono pulse effects.
    
    ChronoPulse - A prefab for chrono pulse effects. Rotates hands of clock. Emits purple pulses from the clock.
    
    MagicCircle, RoF_Beam, RoF_Core - Since FadeOut script adjusted all color of its children, reposition the script and removed unnecessary FadeOut scripts.
    
    ChronoPulseSpawner - A script to spawn chrono pulse effect.
    
    IceMagicCircleRandomSpawner - It spawns magic circles at the location retrieved by GetRandomTransform script.
    
    IceMagicCircleSpawner - A script to spawn magic circle to the given transform.
    
    FadeOut - Changed header comments.
    
    M_ChronoPulse_Wave - Changed parameters to produce better feelings.
    
    SG_ChronoPulse_Waves - Applied better logic to output color and opacity.

--- Project_JM/Assets/Scripts/VFXs/Mage/IceMagicCircleSpawner.cs
+++ Project_JM/Assets/Scripts/VFXs/Mage/IceMagicCircleSpawner.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 02/01/2026 Sinil Kang
+// Copyright (c) 02/08/2026 Sinil Kang
 // Project: Project JM - https://github.com/IDokan/Project_JM
 // File: IceMagicCircleSpawner.cs
 // Summary: A script to spawn Ice magic circle.
@@ -8,18 +8,6 @@ using UnityEngine;
 
 public class IceMagicCircleSpawner : AbstractAnimEventPrefabSpawner<GameObject>
 {
-    [SerializeField] protected GetRandomTransform randomTransform;
-
-    protected void Start()
-    {
-        if(randomTransform == null)
-        {
-            randomTransform = GetComponent<GetRandomTransform>();
-        }
-    }
-
     public void AnimEvent_SpawnMagicCircle()
-    {
-        SpawnAtTransform(randomTransform.RandomTransform());
-    }
+        => Spawn();
 }
