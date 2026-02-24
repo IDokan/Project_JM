f68788cbf6dc3ff91a9bfd47c7af28df555cb352
Sinil Kang <rtd99062@gmail.com>
2026-02-24 15:41:22 +0900

    #27 - Implemented parallax movement to backgrounds.
    SlimeKing - Added EnemyActivation script to enable and disable some scripts such as Enemy AI at instantiation.
    
    ForestBackground - Changed drawing order, added ParallaxLayer and ParallaxLooper scripts to the fence object for sake of tests.
    
    SampleScene - Enabled AI at init enemy since it is not spawned by enemy spawner script. I'm going to handle init progress of the game scene later.
    
    EnemyActivation - A script to enable and disable some scripts.
    
    CameraMover - A script to move camera.
    
    ParallaxLayer - A script to move object by camera location with parallax factor.
    
    ParallaxLooper - A script to move tiles to look it infinite.
    
    BoardManager - Sometimes a bug happened when user requested moving gems right before the board covered. Initialize _numMovingGems everytime all gems cleared.
    
    EnemySpawner - Now it provides to determine time when enemy spawns after a previous one died. Moreover, it also dispatch enemy spawn event after a delay.

---
+++ Project_JM/Assets/Scripts/Action/CameraMover.cs
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 02/23/2026 Sinil Kang
+// Project: Project JM - https://github.com/IDokan/Project_JM
+// File: CameraMover.cs
+// Summary: A script to move camera.
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] protected CharacterDeathEventChannel characterDeathEventChannel;
+
+    [SerializeField] protected float moveDelay = 1f;
+    [SerializeField] protected float moveDistance = 9f;
+    [SerializeField] protected float moveDuration = 3f;
+
+    protected void OnEnable()
+    {
+        characterDeathEventChannel.OnRaised += OnCharacterDied;
+    }
+
+    protected void OnDisable()
+    {
+        characterDeathEventChannel.OnRaised -= OnCharacterDied;
+    }
+
+    protected void OnCharacterDied(CharacterStatus stat)
+    {
+        if (stat.TryGetComponent<EnemyTag>(out _))
+        {
+            Move(moveDistance, moveDuration);
+        }
+    }
+
+    public Coroutine Move(float distance, float seconds)
+    {
+        Vector3 start = transform.position;
+        Vector3 end = start + distance * Vector3.right;
+
+        return StartCoroutine(MoveRoutineAfterDuration(start, end, seconds, moveDelay));
+    }
+
+    protected IEnumerator MoveRoutineAfterDuration(Vector3 start, Vector3 end, float seconds, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (seconds <= 0f)
+        {
+            transform.position = end;
+            yield break;
+        }
+
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector3.Lerp(start, end, t);
+            yield return null;
+        }
+
+        transform.position = end;
+    }
+}
